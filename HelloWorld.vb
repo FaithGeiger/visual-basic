bb6e9b016c5bda4a9c269298c0933e6d80fffa6a
FaithGeiger <faithgeiger345@rusdlearns.net>
2015-09-23 13:29:17 -0700

    Create HelloWorld.vb

---
+++ HelloWorld.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+	Sub Main()
+
+		Console.WriteLine("Hello World")
+		Console.WriteLine("Hello World") 'Console is Class with a Method WriteLine()
+		Console.ReadLine():  'Console is Class with a Method ReadLine()
+
+	End Sub
+	
+End Module

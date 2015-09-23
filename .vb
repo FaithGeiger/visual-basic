3ea844dc08b3054f2bcb873cbda3bc893cd98e9a
FaithGeiger <faithgeiger345@rusdlearns.net>
2015-09-23 13:31:57 -0700

    Create .vb

---
+++ .vb
@@ -0,0 +1,40 @@
+Module Module1
+
+	Sub Main()
+
+		Dim x As Integer 
+		Dim y As Integer
+		x = 7
+		y = x + 3 
+
+		Console.Write(y)
+		
+		'Dim myFirstName As String
+
+		'Dim myFirstName As String 
+		'myFirstName + "Bob"
+		
+		'Dim myFirstName As String = "Bob"
+
+		'Dim myFirstName = "Bob"
+
+
+		Dim myFirstName 
+		Console.WriteLine(myFirstName)
+		
+
+		Dim x As Integer = 7
+		Dim y As String = "Bob"
+		
+		Dim myFirstTry As String = x & y
+		Console.WriteLine(myFirstTry)
+
+		Dim mySecondTry As Integern = x + CInt(y)
+		Console.WriteLine(mySecondTry)
+
+		Console.ReadLine
+		
+		
+		Console.ReadLine()
+	
+End Sub

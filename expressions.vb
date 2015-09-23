8d4b006bbd8d8ab3ccd3899ecde731de0e2a39da
FaithGeiger <faithgeiger345@rusdlearns.net>
2015-09-23 13:42:57 -0700

    Create expressions.vb

---
+++ expressions.vb
@@ -0,0 +1,68 @@
+Module Module1
+    Sub Main()
+
+        Dim x = 0
+        Dim y = 0
+        Dim a = 0
+        Dim b = 0
+
+        ' Assignment operator 
+        x = 3
+
+        'Addition operator
+        x = 3 + 4
+
+        'Subtraction(operator)
+        x = 4 - 3
+
+        'Multiplication operator
+        x = 10 * 5
+
+        'Division operator
+        x = 10 / 5
+
+        'Equality operator
+        If x = y Then
+            'Stuff goes here
+        End If
+
+        'Greater than operator
+        If x > y Then
+            'Stuff goes here
+        End If
+
+        'Less than operator
+        If x < y Then
+            'Stuff goes here
+        End If
+
+        'Greater than equal to operator
+        If x >= y Then
+            'Stuff goes here
+        End If
+
+        'Less thanequal to operator
+        If x <= y Then
+            'Stuff goes here
+        End If
+
+        'Conditional OR operator
+        If x > y Or a > b Then
+            'Stuff goes here
+        End If
+
+        'Conditional AND  operator
+        If x > y And a > b Then
+            'Stuff goes here
+        End If
+
+        'Member access and Method invocation
+        Console.WriteLine("Hil")
+
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module

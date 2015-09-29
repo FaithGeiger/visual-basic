8ab471959c8de064b7f32c376edb3605c21f82cd
FaithGeiger <faithgeiger345@rusdlearns.net>
2015-09-29 13:56:15 -0700

    Create firstprogram.vb

---
+++ firstprogram.vb
@@ -0,0 +1,18 @@
+Module Module1
+
+    Sub Main()
+        Dim userName As String
+        Dim userNumber1 As Double
+        Dim userNumber2 As Double
+        Dim userNumber3 As Double
+
+        Console.WriteLine("Hello and welcome to your first Visual Basic Console Applications" & vbCrLF
+
+        Console.Write("To whom do I have the pleasure of talking with: ")
+        userName = Console.ReadLine()
+
+        Console()
+
+    End Sub
+
+End Module
